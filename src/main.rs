240a575fca6eb3707e57b293fc77e850d442d532
ugr0 <yujiro.takahashi0912@gmail.com>
2022-10-19 15:31:07 +0900

    [chore] added comments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-#![no_std]
-#![no_main]
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop {}
 }
 
